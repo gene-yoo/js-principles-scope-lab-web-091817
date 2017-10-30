38979019654591fefc297fe377a50c8c7caa9498
gene-yoo <gene.y.yoo@gmail.com>
2017-10-30 00:38:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName () {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (newName) {
+  bestCustomer = newName;
+}
+
+const leastFavoriteCustomer = 'me';
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'you';
+}
+
+// function attemptTwoFavoriteCustomers () {
+//   let favoriteCustomer = 'paul';
+//   let favoriteCustomer = 'mike';
+// }
